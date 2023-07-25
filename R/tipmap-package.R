67cd3f95ad723a571a668887404cce07a4223709
Stock <christian.stock@boehringer-ingelheim.com>
2023-07-25 22:47:50 +0200

    File added

---
+++ R/tipmap-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom assertthat assert_that
+#' @importFrom assertthat is.flag
+#' @importFrom assertthat is.scalar
+#' @importFrom magrittr %>%
+#' @importFrom stats approx
+#' @importFrom stats optim
+#' @importFrom stats pbeta
+#' @importFrom stats qnorm
+## usethis namespace: end
+NULL
