aee5b7a853339150f522163d4d833310bccf9694
Stock <christian.stock@boehringer-ingelheim.com>
2023-07-27 02:11:45 +0200

    Minor updates in examples

--- R/fit_beta_mult_exp.R
+++ R/fit_beta_mult_exp.R
@@ -15,14 +15,13 @@
 #' 
 #' @examples
 #' beta_fits <- fit_beta_mult_exp(
-#' chips_mult =
-#'   rbind(
-#'       c(0, 0, 0, 0, 2, 3, 3, 2, 0, 0),
-#'       c(0, 0, 0, 1, 2, 4, 2, 1, 0, 0),
-#'       c(0, 0, 0, 2, 2, 2, 2, 2, 0, 0)
-#'       )
+#'   chips_mult = rbind(
+#'     c(0, 0, 0, 0, 2, 3, 3, 2, 0, 0),
+#'     c(0, 0, 0, 1, 2, 4, 2, 1, 0, 0),
+#'     c(0, 0, 0, 2, 2, 2, 2, 2, 0, 0)
 #'   )
-#' beta_fits
+#' )
+#' print(beta_fits)
 #'
 fit_beta_mult_exp <- function(chips_mult) {
   chips_mult <- purrr::array_branch(array = chips_mult, margin = 1)
--- R/get_summary_mult_exp.R
+++ R/get_summary_mult_exp.R
@@ -11,11 +11,14 @@
 #' @return A vector containing summary statistics.
 #' @export
 #' @examples
-#' get_summary_mult_exp(rbind(
-#'   c(0, 0, 0, 0, 2, 3, 3, 2, 0, 0),
-#'   c(0, 0, 0, 1, 2, 4, 2, 1, 0, 0),
-#'   c(0, 0, 0, 2, 2, 2, 2, 2, 0, 0)
-#'   ))
+#' get_summary_mult_exp(
+#'   chips_mult = rbind(
+#'     c(0, 0, 0, 0, 2, 3, 3, 2, 0, 0),
+#'     c(0, 0, 0, 1, 2, 4, 2, 1, 0, 0),
+#'     c(0, 0, 0, 2, 2, 2, 2, 2, 0, 0)
+#'   ), 
+#'   n = 50
+#' )
 #'
 get_summary_mult_exp <- function(
     chips_mult,
